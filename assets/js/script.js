a226cf968ca3df032059e4508bf3909473e67bfe
Hector Nava <hector.r.nava44@gmail.com>
2021-11-11 15:07:32 -0700

    Added question 3 and 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,19 @@ var btnEl = $('#start-button');
 
 const questions = {
     1: 'Commonly used data types DO NOT include:',
-    2: 'The condition in an if/else statement is enclosed within ____',
-}
+    2: 'The condition in an if/else statement is enclosed within ____.',
+    3: 'Arrays in JavaScript can be used to store ____.',
+    4: 'String value must be enclosed with ____ when being assigned to variables.'
+};
 
 const options = {
     1: ['strings', 'booleans', 'alerts', 'numbers'],
-    2: ['quotes', 'curly brackets', 'parentheses', 'square brackets']
-}
+    2: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    3: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    4: ['commas', 'curly brackets', 'quotes', 'parentheses']
+};
 
-const answers = [3]
+const answers = [3, 3, 4];
 
 btnEl.on('click', function() {
     quesNum = 1;
@@ -44,9 +48,11 @@ btnEl.on('click', function() {
 $(document).on('click', '.btn-child', function(){
     liValues = rootEl.children();
 
+    titleEl.text(questions[quesNum]);
     for (var i = 1; i < liValues.length; i++) {
-        var liEl = $('#'+i)
+        var liEl = $('#'+i);
 
         liEl.text([i+'.', options[quesNum][i-1]].join(' '));
     }
+    quesNum++;
 });
